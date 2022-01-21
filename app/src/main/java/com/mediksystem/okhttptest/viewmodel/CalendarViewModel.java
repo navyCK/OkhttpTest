6384d15b139c6b749d2ad57e280e493083c4d5ac
navyCK <ckck81026@gmail.com>
2022-01-21 14:57:31 +0900

    [master] 캘린더뷰모델

---
+++ app/src/main/java/com/mediksystem/okhttptest/viewmodel/CalendarViewModel.java
@@ -0,0 +1,19 @@
+package com.mediksystem.okhttptest.viewmodel;
+
+import android.widget.TextView;
+
+import com.mediksystem.okhttptest.data.TSLiveData;
+
+import java.util.Calendar;
+
+import androidx.lifecycle.ViewModel;
+
+public class CalendarViewModel extends ViewModel {
+    public TSLiveData<Calendar> mCalendar = new TSLiveData<>();
+
+    public void setCalendar(Calendar calendar) {
+        this.mCalendar.setValue(calendar);
+    }
+
+
+}
