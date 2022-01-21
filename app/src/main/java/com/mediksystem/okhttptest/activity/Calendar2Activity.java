a3a9fff2cfd4f6cbe312a13948c57ed344ab79e2
navyCK <ckck81026@gmail.com>
2022-01-21 14:56:11 +0900

    [master] 캘린더2 액티비티

---
+++ app/src/main/java/com/mediksystem/okhttptest/activity/Calendar2Activity.java
@@ -0,0 +1,15 @@
+package com.mediksystem.okhttptest.activity;
+
+import android.os.Bundle;
+
+import com.mediksystem.okhttptest.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Calendar2Activity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.acitivity_calendar2);
+    }
+}
