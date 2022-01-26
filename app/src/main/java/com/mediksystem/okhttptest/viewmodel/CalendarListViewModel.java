059ea386970ec4356bfc07808603096ce6e1a0c5
navyCK <ckck81026@gmail.com>
2022-01-26 09:45:51 +0900

    [master] 캘린더 크기 수정

--- app/src/main/java/com/mediksystem/okhttptest/viewmodel/CalendarListViewModel.java
+++ app/src/main/java/com/mediksystem/okhttptest/viewmodel/CalendarListViewModel.java
@@ -44,33 +44,43 @@ public class CalendarListViewModel extends ViewModel {
 
     public void setCalendarList(GregorianCalendar cal) {
 
-
         setTitle(cal.getTimeInMillis());
+        Log.e("getTimeInMillis->title", DateFormat.getDate(cal.getTimeInMillis(), DateFormat.CALENDAR_HEADER_FORMAT)); // 밀리초로 나온 값을 년,월로 변환 표시
+        Log.e("cal.getTime()", String.valueOf(cal.getTime()));
 
         ArrayList<Object> calendarList = new ArrayList<>();
-        for (int i = -300; i < 300; i++) {
+
+        // i=1이면 현재날짜월,
+        // i=2이면 현재월+1
+        for (int i = -1; i < 1; i++) {
             try {
                 GregorianCalendar calendar = new GregorianCalendar(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH) + i, 1, 0, 0, 0);
-                Log.e("calendar", String.valueOf(calendar));
+
                 if (i == 0) {
                     mCenterPosition = calendarList.size();
+                    Log.e("mCenterPosition", String.valueOf(mCenterPosition));
                 }
                 calendarList.add(calendar.getTimeInMillis());
 
                 int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK) - 1; //해당 월에 시작하는 요일 -1 을 하면 빈칸을 구할 수 있겠죠 ?
                 int max = calendar.getActualMaximum(Calendar.DAY_OF_MONTH); // 해당 월에 마지막 요일
+                Log.e("dayOfWeek", i+String.valueOf(dayOfWeek));
+                Log.e("max", i+String.valueOf(max));
 
 
                 for (int j = 0; j < dayOfWeek; j++) {
                     calendarList.add(Keys.EMPTY);
+
                 }
                 for (int j = 1; j <= max; j++) {
                     calendarList.add(new GregorianCalendar(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), j));
+
                 }
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
+
         mCalendarList.setValue(calendarList);
 
     }
