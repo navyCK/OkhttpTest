7d04b1383c2bdd45dfe14d3c6c481156bc81ada7
navyCK <ckck81026@gmail.com>
2022-01-28 09:29:07 +0900

    [master] 불필요 코드 제거

--- app/src/main/java/com/mediksystem/okhttptest/adapter/CalendarAdapter.java
+++ app/src/main/java/com/mediksystem/okhttptest/adapter/CalendarAdapter.java
@@ -1,7 +1,10 @@
 package com.mediksystem.okhttptest.adapter;
 
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.mediksystem.okhttptest.R;
@@ -145,5 +148,7 @@ public class CalendarAdapter extends ListAdapter<Object, RecyclerView.ViewHolder
             binding.setModel(model);
             binding.executePendingBindings();
         }
+
+
     }
 }
