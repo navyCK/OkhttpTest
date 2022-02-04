d87def74d7dbf4e5fa73dbce6fe7968788816465
navyCK <ckck81026@gmail.com>
2022-02-04 14:42:37 +0900

    [master] 임시 액티비티

---
+++ calendar/src/main/java/com/mediksystem/calendar/MainActivity.java
@@ -0,0 +1,14 @@
+package com.mediksystem.calendar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
