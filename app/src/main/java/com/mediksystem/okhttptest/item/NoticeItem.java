c8d075101275f4c878f1f549931f7a9ce3e8c10f
navyCK <ckck81026@gmail.com>
2022-02-08 13:04:55 +0900

    [master] 공지 아이템

---
+++ app/src/main/java/com/mediksystem/okhttptest/item/NoticeItem.java
@@ -0,0 +1,36 @@
+package com.mediksystem.okhttptest.item;
+
+import android.graphics.drawable.Drawable;
+
+public class NoticeItem {
+    private Drawable iconDrawable ;
+    private String titleStr ;
+    private String descStr ;
+    private String dateStr ;
+
+    public void setIcon(Drawable icon) {
+        iconDrawable = icon ;
+    }
+    public void setTitle(String title) {
+        titleStr = title ;
+    }
+    public void setDesc(String desc) {
+        descStr = desc ;
+    }
+    public void setDate(String date) {
+        dateStr = date ;
+    }
+
+    public Drawable getIcon() {
+        return this.iconDrawable ;
+    }
+    public String getTitle() {
+        return this.titleStr ;
+    }
+    public String getDesc() {
+        return this.descStr ;
+    }
+    public String getDate() {
+        return this.dateStr ;
+    }
+}
